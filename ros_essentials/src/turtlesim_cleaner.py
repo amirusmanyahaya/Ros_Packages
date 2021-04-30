849c7c04ad298e93c81f51d010b5b308e0ad264c
Ameer Usman <amirusman20@gamil.com>
2021-04-30 05:19:30 +0200

    implemented move to point

--- ros_essentials/src/turtlesim_cleaner.py
+++ ros_essentials/src/turtlesim_cleaner.py
@@ -8,16 +8,18 @@ import sys
 import math
 
 
-distance = 0.0
-theta = 0.0
+x = 0.0
+y = 0.0
+yaw = 0.0
 # using pose to update the distance covered
 def callback(pose):
-    global distance, theta
-    distance = pose.x
-    theta = pose.theta
+    global x, y, yaw
+    x = pose.x
+    y = pose.x
+    yaw = pose.theta
 
 def move(velocity_publisher,speed,stop_x,is_forward):
-    global distance
+    global x
     velocity = Twist()
     if is_forward:
         velocity.linear.x = speed
@@ -27,11 +29,10 @@ def move(velocity_publisher,speed,stop_x,is_forward):
     while not rospy.is_shutdown():
         velocity_publisher.publish(velocity)
         rate.sleep()
-        if distance >= stop_x:
+        if x >= stop_x:
             rospy.loginfo("Gotten to my destination")
             break
 def rotate(velocity_publisher,ang_speed,rotation_ang, clockwise):
-    global theta
     velocity = Twist()
     # convert to radians    
     ang_speed = math.radians(ang_speed)
@@ -41,12 +42,44 @@ def rotate(velocity_publisher,ang_speed,rotation_ang, clockwise):
         velocity.angular.z = ang_speed
     
     rate = rospy.Rate(100)
+    t0 = rospy.Time().now().to_sec()
     while not rospy.is_shutdown():
         velocity_publisher.publish(velocity)
         rate.sleep()
-        if theta >= math.radians(rotation_ang):
+        t1 = rospy.Time().now().to_sec()
+        current_angle = (t1 - t0) * ang_speed
+        if current_angle > math.radians(rotation_ang):
             rospy.loginfo("rotated to the destination")
             break
+    velocity.angular.z = 0.0
+    velocity_publisher.publish(velocity)
+
+def move_to_point(publisher, end_x, end_y):
+    global x, y, yaw
+    velocity = Twist()
+
+    K_linear = 0.5
+    K_angular = 4.0
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        distance = math.sqrt((end_x - x)**2 + (end_y - y)**2)
+        linear_speed = distance * K_linear
+        desired_ang = math.atan2(end_y - y, end_x - x)
+        angular_speed = (desired_ang-yaw) * K_angular
+
+        velocity.linear.x = linear_speed
+        velocity.angular.z = angular_speed
+
+        publisher.publish(velocity)
+        rate.sleep()
+
+        rospy.loginfo(f"distance: {distance}, x: {x}, y: {y}, yaw: {yaw}")
+
+        if(distance < 0.01):
+            break
+
 
 if __name__ == '__main__':
     try:
@@ -55,7 +88,8 @@ if __name__ == '__main__':
         # speed,stop_x = (float(sys.argv[1]),float(sys.argv[2]))
         rospy.Subscriber('/turtle1/pose', Pose, callback)
         rospy.sleep(2)
-        # move(velocity_publisher, speed , stop_x, is_forward=True)
-        rotate(velocity_publisher, 5.0, 180, clockwise=False)
+        # move(velocity_publisher, 3.0 , 7.0, is_forward=True)
+        # rotate(velocity_publisher, 5.0, 180, clockwise=False)
+        move_to_point(velocity_publisher,5.0, 5.0)
     except rospy.ROSInterruptException:
         pass
